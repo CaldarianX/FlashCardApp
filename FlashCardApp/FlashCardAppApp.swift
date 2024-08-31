cf126e0f4c1f384be565fb330c90bbc8f04b3840
Tanakit <153923689+CaldarianZ@users.noreply.github.com>
2024-08-31 07:50:47 +0700

    Initial Commit

---
+++ FlashCardApp/FlashCardAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlashCardAppApp.swift
+//  FlashCardApp
+//
+//  Created by Raphael on 31/8/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlashCardAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
